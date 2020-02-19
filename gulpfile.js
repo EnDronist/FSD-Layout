9bd4f134daf67aedb915cadd1e9fa84261c11d40
EnDronist <jarweese@gmail.com>
2020-02-19 15:22:05 +1000

    Removed all absolute elements positions and added '_*.scss' files for data support

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ sass.compiler = require('sass');
 
 // Compiling SASS(scss) into CSS
 gulp.task('sass', function() {
-    return gulp.src(['./**/*.scss', '!./node_modules/**']) // Get source files
+    return gulp.src(['./**/*.scss', '!./**/_*.scss', '!./node_modules/**']) // Get source files
         .pipe(sass()) // Compiles with sass
         .pipe(gulp.dest('.')); // Outputs the file in the destination folder
 });
@@ -29,6 +29,6 @@ gulp.task('ejs', function() {
 
 // Compiling with watch mode
 gulp.task('watch', function() {
-    gulp.watch(['./**/*.scss', '!./node_modules/**'], gulp.series('sass'));
+    gulp.watch(['./**/*.scss', '!./**/_*.scss', '!./node_modules/**'], gulp.series('sass'));
     gulp.watch(['./**/*.ejs', '!./node_modules/**'], gulp.series('ejs'));
 });
